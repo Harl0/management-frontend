71745b8b62e89067edff21af2f0e97de1d0ff1a7
Basit Pathan <basit.pathan@digital.hmrc.gov.uk>
2017-06-21 15:08:23 +0100

    removed healthcheck

--- app/controllers/HealthCheckController.scala
+++ app/controllers/HealthCheckController.scala
@@ -1,9 +1,9 @@
-package controllers
-
-import play.api.mvc._
-
-class HealthCheckController extends InjectedController {
-
-  def healthCheck: Action[AnyContent] = Action { Ok }
-
-}
+//package controllers
+//
+//import play.api.mvc._
+//
+//class HealthCheckController extends InjectedController {
+//
+//  def healthCheck: Action[AnyContent] = Action { Ok }
+//
+//}
