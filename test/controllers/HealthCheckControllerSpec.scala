72c6af8cb945a4ea73a06874f88d7812f090e0bd
Basit Pathan <basit.pathan@digital.hmrc.gov.uk>
2017-06-21 12:50:11 +0100

    healthcheck endpoint

---
+++ test/controllers/HealthCheckControllerSpec.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.test.Helpers._
+import play.api.test._
+
+
+class HealthCheckControllerSpec extends PlaySpec with GuiceOneAppPerTest {
+
+  "The healthcheck endpoint" must {
+    "respond with a 200 status code when the service is OK" in {
+      status(route(app, FakeRequest("GET", "/healthcheck")).get) must be (200)
+    }
+  }
+
+
+}
