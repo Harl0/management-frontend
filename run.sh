53ff5ba9dae9d38c42c1f07fdb1012197737d3f5
Basit Pathan <basit.pathan@digital.hmrc.gov.uk>
2017-07-12 10:20:25 +0100

    host.address jvm arg

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 aws s3 --region eu-west-2 cp s3://basappconfigbucket/$ENVIRONMENT/$MSNAME.conf .
-java -Dhttp.port=8080 -Dconfig.file=$MSNAME.conf -jar /opt/$MSNAME.jar
+java -Dhttp.port=8080 -Dhttp.address=0.0.0.0 -Dconfig.file=$MSNAME.conf -jar /opt/$MSNAME.jar
