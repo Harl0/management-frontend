a325dbeb004bea3807666a2ac3f495223b4a67cd
Basit Pathan <basit.pathan@digital.hmrc.gov.uk>
2017-07-15 19:45:15 +0100

    removed console logs

--- public/javascripts/deleteClient.js
+++ public/javascripts/deleteClient.js
@@ -1,6 +1,3 @@
-/**
- * Created by jason on 04/07/17.
- */
 $(document).ready(function () {
     $("#delete-all").change(function () {
         $("input[value='delete']:checkbox").prop('checked', $(this).prop("checked"));
@@ -9,18 +6,15 @@ $(document).ready(function () {
     $('#delete-submit').click(function () {
         var $inputs = $("td input[name='delete']:checked");
         var count = $inputs.length;
-
         if (confirm('Are you sure you want to delete ' + count + ' client(s)')) {
             $inputs.each(function () {
                 var id = $(this).parent().siblings().first().text();
-                console.log("This ID is " + id);
                 $.ajax({
                     contentType: 'application/json',
                     method: 'GET',
                     url: '/management/client/delete?_id=' + id,
                     data: JSON.stringify([id]),
                     success: function (data, status) {
-                        console.log(data, status);
                         window.location.replace('/management/client/list');
                     },
                     error: function (req, status, error) {
