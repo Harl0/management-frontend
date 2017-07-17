7dd52e3500a91fcecaa2d5a3aa556c58404ad4cc
Basit Pathan <basit.pathan@digital.hmrc.gov.uk>
2017-07-17 11:51:29 +0100

    jvm args

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 aws s3 --region eu-west-2 cp s3://basappconfigbucket/$ENVIRONMENT/$MSNAME.conf .
-java -Xms128m -Xmx128m -XX:+UseG1GC -Dhttp.port=8080 -Dhttp.address=0.0.0.0 -Dconfig.file=$MSNAME.conf -jar /opt/$MSNAME.jar
+java $JAVA_ARGS -jar /opt/$MSNAME.jar
