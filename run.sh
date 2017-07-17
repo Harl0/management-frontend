b6f6b6a80bcdadff14988d83f1efff8e1a488e9c
Basit Pathan <basit.pathan@digital.hmrc.gov.uk>
2017-07-17 12:33:08 +0100

    jvm args

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-aws s3 --region eu-west-2 cp s3://basappconfigbucket/$ENVIRONMENT/$MSNAME.conf .
+aws s3 --region eu-west-2 cp s3://basappconfigbucket/services/$ENVIRONMENT/$MSNAME.conf .
 java $JAVA_ARGS -jar /opt/$MSNAME.jar
