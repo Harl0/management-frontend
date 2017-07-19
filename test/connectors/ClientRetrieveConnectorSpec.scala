d9c1764db1dad8fc195ea1d54689838dfe16525a
yang.wong <yang.wong@digital.hmrc.gov.uk>
2017-07-19 10:56:15 +0100

    BAS-380 added unit tests for connectors

--- test/connectors/ClientRetrieveConnectorSpec.scala
+++ test/connectors/ClientRetrieveConnectorSpec.scala
@@ -4,8 +4,7 @@ import akka.stream.Materializer
 import config.AppConfig
 import helpers.ErrorHelper
 import mockws.{MockWS, Route}
-import models.ClientForm._
-import models.ClientRegistrationResponse
+import models.{Client, ClientRegistrationResponse}
 import org.mockito.Matchers
 import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
@@ -26,25 +25,25 @@ class ClientRetrieveConnectorSpec extends PlaySpec
   val mockErrorHelper = mock[ErrorHelper]
   val defaultDuration = 5
 
-  "createClient" should {
-    "create client given a success is returned" in {
+  "retrieveClientDetail" should {
+    "retrieve a client given a success is returned" in {
 
       val userRoute: Route = Route {
-        case ("POST", u) => Action {
-          Ok(Json.toJson(ClientRegistrationResponse("chaz","dingle","bob")))
+        case ("GET", u) => Action {
+          Ok(Json.toJson(Client("chaz","dingle","berry","bob","cat")))
         }
       }
 
-      def ageResponse(): Either[String, ClientRegistrationResponse] = {
+      def ageResponse(): Either[String, Client] = {
         // we initialize a mock WS with the defined route
         val ws = MockWS(userRoute)
         // we inject the MockWS into GatewayToTest
-        val testedGateway = new ClientUpdateConnector(ws, mockAppConfig, mockErrorHelper)
-        try Await.result(testedGateway.createClient(ClientRegistrationForm("chaz","dingle")),Duration.apply(defaultDuration, SECONDS))
+        val testedGateway = new ClientRetrieveConnector(ws, mockAppConfig, mockErrorHelper)
+        try Await.result(testedGateway.retrieveClientDetail("chaz"),Duration.apply(defaultDuration, SECONDS))
         finally ws.close()
       }
 
-      ageResponse mustBe Right(ClientRegistrationResponse("chaz","dingle","bob"))
+      ageResponse mustBe Right(Client("chaz","dingle","berry","bob","cat"))
     }
 
     "return error given an error is returned" in {
@@ -53,17 +52,17 @@ class ClientRetrieveConnectorSpec extends PlaySpec
         .thenReturn("fatal chaz dingle error")
 
       val userRoute: Route = Route {
-        case ("POST", u) => Action {
+        case ("GET", u) => Action {
           InternalServerError("dingle chaz")
         }
       }
 
-      def ageResponse(): Either[String, ClientRegistrationResponse] = {
+      def ageResponse(): Either[String, Client] = {
         // we initialize a mock WS with the defined route
         val ws = MockWS(userRoute)
         // we inject the MockWS into GatewayToTest
-        val testedGateway = new ClientUpdateConnector(ws, mockAppConfig, mockErrorHelper)
-        try Await.result(testedGateway.createClient(ClientRegistrationForm("chaz","dingle")),Duration.apply(defaultDuration, SECONDS))
+        val testedGateway = new ClientRetrieveConnector(ws, mockAppConfig, mockErrorHelper)
+        try Await.result(testedGateway.retrieveClientDetail("chaz"),Duration.apply(defaultDuration, SECONDS))
         finally ws.close()
       }
 
@@ -73,54 +72,54 @@ class ClientRetrieveConnectorSpec extends PlaySpec
     //TODO test for exception
 
     }
-//
-//  "updateClient" should {
-//    "create client given a success is returned" in {
-//
-//      val userRoute: Route = Route {
-//        case ("POST", u) => Action {
-//          Ok(Json.toJson(ClientRegistrationResponse("chaz","dingle","bob")))
-//        }
-//      }
-//
-//      def ageResponse(): Either[String, ClientRegistrationResponse] = {
-//        // we initialize a mock WS with the defined route
-//        val ws = MockWS(userRoute)
-//        // we inject the MockWS into GatewayToTest
-//        val testedGateway = new ClientUpdateConnector(ws, mockAppConfig, mockErrorHelper)
-//        try Await.result(testedGateway.createClient(ClientRegistrationForm("chaz","dingle")),Duration.apply(defaultDuration, SECONDS))
-//        finally ws.close()
-//      }
-//
-//      ageResponse mustBe Right(ClientRegistrationResponse("chaz","dingle","bob"))
-//    }
-//
-//    "return error given an error is returned" in {
-//
-//      when(mockErrorHelper.non200Error(Matchers.any[Int],Matchers.any[String]))
-//        .thenReturn("fatal chaz dingle error")
-//
-//      val userRoute: Route = Route {
-//        case ("POST", u) => Action {
-//          InternalServerError("dingle chaz")
-//        }
-//      }
-//
-//      def ageResponse(): Either[String, ClientRegistrationResponse] = {
-//        // we initialize a mock WS with the defined route
-//        val ws = MockWS(userRoute)
-//        // we inject the MockWS into GatewayToTest
-//        val testedGateway = new ClientUpdateConnector(ws, mockAppConfig, mockErrorHelper)
-//        try Await.result(testedGateway.createClient(ClientRegistrationForm("chaz","dingle")),Duration.apply(defaultDuration, SECONDS))
-//        finally ws.close()
-//      }
-//
-//      ageResponse mustBe Left("fatal chaz dingle error")
-//    }
-//
-//    //TODO test for exception
-//
-//  }
+
+  "retrieveClientList" should {
+    "retrieve a client list given a success is returned" in {
+
+      val userRoute: Route = Route {
+        case ("GET", u) => Action {
+          Ok(Json.toJson(Seq(Client("chaz","dingle","berry","bob","cat"))))
+        }
+      }
+
+      def ageResponse(): Either[String, Seq[Client]] = {
+        // we initialize a mock WS with the defined route
+        val ws = MockWS(userRoute)
+        // we inject the MockWS into GatewayToTest
+        val testedGateway = new ClientRetrieveConnector(ws, mockAppConfig, mockErrorHelper)
+        try Await.result(testedGateway.retrieveClientList,Duration.apply(defaultDuration, SECONDS))
+        finally ws.close()
+      }
+
+      ageResponse mustBe  Right(List(Client("chaz","dingle","berry","bob","cat",None,None,None,None)))
+    }
+
+    "return error given an error is returned" in {
+
+      when(mockErrorHelper.non200Error(Matchers.any[Int],Matchers.any[String]))
+        .thenReturn("fatal chaz dingle error")
+
+      val userRoute: Route = Route {
+        case ("GET", u) => Action {
+          InternalServerError("dingle chaz")
+        }
+      }
+
+      def ageResponse(): Either[String, Seq[Client]] = {
+        // we initialize a mock WS with the defined route
+        val ws = MockWS(userRoute)
+        // we inject the MockWS into GatewayToTest
+        val testedGateway = new ClientRetrieveConnector(ws, mockAppConfig, mockErrorHelper)
+        try Await.result(testedGateway.retrieveClientList,Duration.apply(defaultDuration, SECONDS))
+        finally ws.close()
+      }
+
+      ageResponse mustBe Left("fatal chaz dingle error")
+    }
+
+    //TODO test for exception
+
+  }
 
 }
 
